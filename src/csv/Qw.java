11cd6634cc131e1a1353f311658a8ad0cc070eaa
playmaker7 <mlemishka@mail.ru>
2013-11-01 13:11:49 +0200

    initial commit

---
+++ src/csv/Qw.java
@@ -0,0 +1,14 @@
+package csv;
+
+import java.io.FileNotFoundException;
+
+import com.csvreader.CsvReader;
+import com.csvreader.CsvWriter;
+
+public class Qw {
+	public static void p() throws FileNotFoundException{
+		System.out.println("wwwwwwwwww");
+		CsvReader r=new CsvReader("ggg");
+	}
+
+}
